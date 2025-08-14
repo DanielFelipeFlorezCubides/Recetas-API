d230faf714bbf592ada36a0e62a070331469b907
BreynerFernandoPintoCardenas <186078942+BreynerFernandoPintoCardenas@users.noreply.github.com>
2025-08-13 23:50:20 -0500

    feat: :construction: Se creo la funcion de obtener por ID, se sigue trabajando en la funcion CREATE

--- routers/recetaRouter.js
+++ routers/recetaRouter.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
 import { getDB } from "../db/config.js";
 
-const route = Router()
+const router = Router()
 
-route.get("/getall", async function (req, res) {
+router.get("/getall", async function (req, res) {
     try{
         const recetas = await getDB().collection("recetas").find().toArray()
         res.status(200).json(recetas)
@@ -11,6 +11,37 @@ route.get("/getall", async function (req, res) {
         res.status(500).json({error: "Internal server error"})
     }
 })
+router.get("/get/:id", async function (req, res) {
+  try {
+    const idReceta = parseInt(req.params.id);
+    const receta = await getDB().collection("recetas").findOne({ id: idReceta });
+    if (!receta) {
+      res.status(404).json({ error: "Receta doesn't exists!" });
+    }
+    res.status(200).json(receta);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+router.post("/create", async function (req, res) {
+  try {
+    const { id, titulo, clientID, descripcion, ingredientes } = req.body;
+    if (!id || !nombre || !habilidad || !descripcion || !ingredientes === undefined) {
+      res.status(400).json({ error: "Invalid asdasdsadsad!" });
+    }
+    const receta = {
+      id,
+      clientID,
+      titulo,
+      descripcion,
+      ingredientes
+    };
+    await getDB().collection("recetas").insertOne(receta);
+    res.status(201).json({ message: "Receta created!!" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal servasdasdsadasdsaer error" });
+  }
+});
 
 
-export default route;
+export default router;
